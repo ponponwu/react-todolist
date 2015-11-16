18c6bb87d80b0d318aa29729c95e02662c8c6d37
ponpon <sheep0406@hotmail.com>
2015-11-16 08:15:07 +0800

    do practice but failed

---
+++ app/scripts/index.js
@@ -0,0 +1,18 @@
+
+
+var Table = React.createClass({
+    render: function() {
+    return (
+      <div>        
+        <h3>ReactJS 101</h3>
+      </div>
+    );
+  }
+});
+
+
+React.render(
+
+  <Table />,
+  document.getElementById('app')
+);
