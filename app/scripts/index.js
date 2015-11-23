764041b19d54d86de4e7ab944eec37e8b6d24021
ponpon <sheep0406@hotmail.com>
2015-11-23 23:43:33 +0800

    Add add func for temp

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,10 +1,36 @@
+var Todolist = React.createClass({
+  createItem: function(itemText){
+    return <li> {itemText} </li>
 
+  },
+  render: function(){
+    return <ul>{this.props.items.map(this.createItem)}</ul>;
+  }
 
-var Table = React.createClass({
-    render: function() {
-    return (
-      <div>        
-        <h3>ReactJS 101</h3>
+});
+
+var TodoApp = React.createClass({
+  getInitialState: function(){
+    return {items: [], text: ''};
+  },
+  handleChange: function(e){
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function(e){
+    e.preventDefault();
+    var nextItems = this.state.items.concat([this.state.text]);
+    var nextText = '';
+    this.setState({items: nextItems, text: nextText});
+  },
+  render: function(){
+    return(
+      <div>
+        <h3>TodoList</h3>
+        <Todolist items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} value = {this.state.text} />
+          <button class="btn btn-danger" >{'Add #' + (this.state.items.length+1)}</button>
+        </form>
       </div>
     );
   }
@@ -12,7 +38,6 @@ var Table = React.createClass({
 
 
 React.render(
-
-  <Table />,
+  <TodoApp />,
   document.getElementById('app')
 );
